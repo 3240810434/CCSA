531856c7f6571c6d8fec3a79b10a7bd512fd2e06
3240810434 <3240810434@qq.com>
2025-12-21 23:33:36 +0800

    1

--- app/src/main/java/com/gxuwz/ccsa/api/ApiService.java
+++ app/src/main/java/com/gxuwz/ccsa/api/ApiService.java
@@ -2,19 +2,22 @@ package com.gxuwz.ccsa.api;
 
 import com.gxuwz.ccsa.model.Admin;
 import com.gxuwz.ccsa.model.User;
-import com.gxuwz.ccsa.common.Result; // 下面会创建这个类
+import com.gxuwz.ccsa.common.Result;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.POST;
 
 public interface ApiService {
 
+    // 对应后端的 /api/admin/login
     @POST("api/admin/login")
     Call<Result<Admin>> adminLogin(@Body Admin admin);
 
+    // 对应后端的 /api/user/login
     @POST("api/user/login")
     Call<Result<User>> userLogin(@Body User user);
 
+    // 对应后端的 /api/user/register
     @POST("api/user/register")
     Call<Result<User>> userRegister(@Body User user);
 }
--- app/src/main/java/com/gxuwz/ccsa/api/RetrofitClient.java
+++ app/src/main/java/com/gxuwz/ccsa/api/RetrofitClient.java
@@ -4,8 +4,11 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
-    // 修改为你的当前 IP
+    // 确保这里的 IP 是你电脑当前的局域网 IP
+    // 确保端口是 8080
+    // 确保最后有一个斜杠 /
     private static final String BASE_URL = "http://192.168.115.251:8080/";
+
     private static RetrofitClient instance;
     private ApiService apiService;
 
